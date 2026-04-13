0337f618a199893fd2ee8fb175e0808ef287feaa
zuck_pa <zuckpa1999@gmail.com>
2026-04-13 14:55:40 +0700

    chore: update initialData

--- src/initialData.ts
+++ src/initialData.ts
@@ -1,62 +1,93 @@
-export const initialData = [
-    {
-        "name": "One Piece Live Action Season 2",
-        "imageUrl": "https://preview.redd.it/new-poster-for-one-piece-live-action-season-2-into-the-v0-sjo4b1wsqptf1.jpeg?width=640&crop=smart&auto=webp&s=57393dbf323e47776932073fe1e1548dd3945f38",
-        "dateWatched": "2026-03-20",
-        "rating": 5,
-        "thoughts": "Love it! Every episode comes with crazy graphic mostly for devil fruit power and make a fantasy part of One piece become a reality e.g Laboon / Giant warriors. The show makes sure that the cool / surreal animation is a part of story telling not exagerrating over the main plot lines. Some aspects of the characters are being adapted gracefully to fit the cotext of live action. Well-executed in my opinion e.g Sanji's obsession over women, Mihawk's beawness",
-        "type": "TV Series",
-        "id": "3b05202d-c2e2-4f6d-bdc8-a2258adb7d2f",
-        "createdAt": "2026-03-22T08:15:16.673Z"
-    },
-    {
-        "name": "Innocent thing",
-        "imageUrl": "https://mibih.wordpress.com/wp-content/uploads/2015/04/innocent_thing.jpg",
-        "dateWatched": "2026-03-08",
-        "rating": 3,
-        "thoughts": "I thought it was a gonna be romcom lol. it turned out to be thriller + erotic. I was shocked with how people were killed out of no where. It was alright. The progatonist is a bit bored with his 'adult life', so the girl somehow become a dangerous yet exciting toy for him. The situation got complicated as the wife of the protagonist found out the situation.",
-        "type": "Movie",
-        "id": "138642ac-82d6-48ae-ab3a-4ba4ed0742e7",
-        "createdAt": "2026-03-11T17:26:54.168Z"
-    },
-    {
-        "name": "War Machine",
-        "imageUrl": "https://www.hollywoodreporter.com/wp-content/uploads/2026/03/WM_Unit_26235_R-H-2026.jpg?w=1296&h=730&crop=1",
-        "dateWatched": "2026-03-10",
-        "rating": 4,
-        "thoughts": "This movie is a classic scifi movie. The plot is simple. The progatonist has got a scarred memories and he wants to join the elite military group to somehow make himself feel better about the past event. A lot of military training. Honestly, this movie reminds me how much soft power the US possesses with  movies/ TV series portraying FBI/CIA as the 'good guy' of the world. Trump, unfortunately, worsen how people perceive US a bit",
-        "type": "Movie",
-        "id": "879c3e9d-e072-49bd-9db2-eb8ee36dcbf4",
-        "createdAt": "2026-03-11T17:25:07.253Z"
-    },
-    {
-        "name": "Hoppers",
-        "imageUrl": "https://lumiere-a.akamaihd.net/v1/images/hoppers-teaserposter-th_7df3546f.jpeg?region=0%2C0%2C734%2C1100",
-        "dateWatched": "2026-03-11",
-        "rating": 5,
-        "thoughts": "The beavers are so cute! Honestly I had never heard of a beaver before aside from beaverton the city hahah. The movie is engaging from start to finish. The pacing is perfect. not too rush not to dragged out. The graphic especially with the  water and the fire are spectacular. There are many scene that's extremely hilarious 😆. Another masterpiece from Pixar",
-        "type": "Movie",
-        "id": "852141ad-7116-45f9-b269-ade35d639240",
-        "createdAt": "2026-03-11T17:12:53.904Z"
-    },
-    {
-        "name": "GUNDAM HATHAWAY The Sorcery of Nymph Circe",
-        "imageUrl": "https://gundam-official.com/media/hathaway_ii_kv_5318aad7e9/hathaway_ii_kv_5318aad7e9.png",
-        "dateWatched": "2026-03-07",
-        "rating": 4.5,
-        "thoughts": "The 2nd movie of Gundam Hathaway. The story revolves around Hathaway's conflict between idealism and Pragmaticism. The graphic and animation is insanely beautiful. The easter egg during the final fight is also a great payoff for a UC fan. Definitely a must-watch if you are a gundam nerd",
-        "type": "Anime",
-        "id": "1e073885-f894-48ea-886a-7fd9dcc8e61b",
-        "createdAt": "2026-03-07T07:49:17.090Z"
-    },
-    {
-        "name": "A knight of the seven kingdoms",
-        "imageUrl": "https://media.gq.com/photos/69863f3db7871ca31241eaf9/16:9/w_2560%2Cc_limit/dexter-sol-ansell-peter-claffey.jpg",
-        "dateWatched": "2026-03-02",
-        "rating": 5,
-        "thoughts": "A prequel from GOT. GOT/HotD usually tell a story from the Lord or the noble perspective. In contrary, this series is about a commoner Dunk whose master died unexpectedly and in searching of an honor to serve/ to be recognized in 7 Kingdom.",
-        "type": "TV Series",
-        "id": "de186aa8-00fa-43ae-a299-d59a8a391e86",
-        "createdAt": "2026-03-07T07:46:23.842Z"
-    }
-]
+export const initialData =
+    [
+        {
+            "name": "Rounders",
+            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT28vot76uVDRSxAaa4aOaDhX5h-Bzg267vCLusXQRXZ6lhiceC4ObIKwSdJrnROkfe_DYM&s=10",
+            "dateWatched": "2026-04-01",
+            "rating": 5,
+            "thoughts": "Holy shit I really like Matt Damon. something about his face when he was young coupled with the 80s to 90s style of film. there was some beauty to it. The plot was simple and straightforward yet keep me interested until the end.  I felt the vibe from Good will hunting as well",
+            "type": "Movie",
+            "id": "f26f6d81-dee9-4f9c-8611-f6a0a8c7bdf7",
+            "createdAt": "2026-04-13T07:53:45.812Z"
+        },
+        {
+            "name": "Dear X",
+            "imageUrl": "https://ptcdn.info/korea/2025/poster/iLMDPiY999-1760951497_o.jpg",
+            "dateWatched": "2026-04-10",
+            "rating": 4.5,
+            "thoughts": "Pyschotic women do things to advance in life fueled by her traumatized past. keep me hooked from the 1st episode to the last which is not typical for korean TV series which usually a few episodes to ramp up",
+            "type": "TV Series",
+            "id": "1786d3b5-eda3-423d-ab83-46a7227d5334",
+            "createdAt": "2026-04-13T07:48:17.565Z"
+        },
+        {
+            "name": "Project Hail Mary",
+            "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtQ0a6OyQAwsiyJwHEGxQYNMNbosB7w2w4P8bXTb-mqYopvfTkiH80v2hPYtHz5OiydoMn&s=10",
+            "dateWatched": "2026-04-12",
+            "rating": 5,
+            "thoughts": "Gave me interstellar vibe. I liked it. The movie made me feel all kinds of emotions unlike the other space movies. Survival in space. Bravery. Wholesome friendship. comradery with Happy Ending.",
+            "type": "Movie",
+            "id": "b36bce9d-b651-47e4-8e6e-08247f5f5427",
+            "createdAt": "2026-04-13T07:45:26.969Z"
+        },
+        {
+            "name": "One Piece Live Action Season 2",
+            "imageUrl": "https://preview.redd.it/new-poster-for-one-piece-live-action-season-2-into-the-v0-sjo4b1wsqptf1.jpeg?width=640&crop=smart&auto=webp&s=57393dbf323e47776932073fe1e1548dd3945f38",
+            "dateWatched": "2026-03-20",
+            "rating": 5,
+            "thoughts": "Love it! Every episode comes with crazy graphic mostly for devil fruit power and make a fantasy part of One piece become a reality e.g Laboon / Giant warriors. The show makes sure that the cool / surreal animation is a part of story telling not exagerrating over the main plot lines. Some aspects of the characters are being adapted gracefully to fit the cotext of live action. Well-executed in my opinion e.g Sanji's obsession over women, Mihawk's beawness",
+            "type": "TV Series",
+            "id": "3b05202d-c2e2-4f6d-bdc8-a2258adb7d2f",
+            "createdAt": "2026-03-22T08:15:16.673Z"
+        },
+        {
+            "name": "Innocent thing",
+            "imageUrl": "https://mibih.wordpress.com/wp-content/uploads/2015/04/innocent_thing.jpg",
+            "dateWatched": "2026-03-08",
+            "rating": 3,
+            "thoughts": "I thought it was a gonna be romcom lol. it turned out to be thriller + erotic. I was shocked with how people were killed out of no where. It was alright. The progatonist is a bit bored with his 'adult life', so the girl somehow become a dangerous yet exciting toy for him. The situation got complicated as the wife of the protagonist found out the situation.",
+            "type": "Movie",
+            "id": "138642ac-82d6-48ae-ab3a-4ba4ed0742e7",
+            "createdAt": "2026-03-11T17:26:54.168Z"
+        },
+        {
+            "name": "War Machine",
+            "imageUrl": "https://www.hollywoodreporter.com/wp-content/uploads/2026/03/WM_Unit_26235_R-H-2026.jpg?w=1296&h=730&crop=1",
+            "dateWatched": "2026-03-10",
+            "rating": 4,
+            "thoughts": "This movie is a classic scifi movie. The plot is simple. The progatonist has got a scarred memories and he wants to join the elite military group to somehow make himself feel better about the past event. A lot of military training. Honestly, this movie reminds me how much soft power the US possesses with  movies/ TV series portraying FBI/CIA as the 'good guy' of the world. Trump, unfortunately, worsen how people perceive US a bit",
+            "type": "Movie",
+            "id": "879c3e9d-e072-49bd-9db2-eb8ee36dcbf4",
+            "createdAt": "2026-03-11T17:25:07.253Z"
+        },
+        {
+            "name": "Hoppers",
+            "imageUrl": "https://lumiere-a.akamaihd.net/v1/images/hoppers-teaserposter-th_7df3546f.jpeg?region=0%2C0%2C734%2C1100",
+            "dateWatched": "2026-03-11",
+            "rating": 5,
+            "thoughts": "The beavers are so cute! Honestly I had never heard of a beaver before aside from beaverton the city hahah. The movie is engaging from start to finish. The pacing is perfect. not too rush not to dragged out. The graphic especially with the  water and the fire are spectacular. There are many scene that's extremely hilarious 😆. Another masterpiece from Pixar",
+            "type": "Movie",
+            "id": "852141ad-7116-45f9-b269-ade35d639240",
+            "createdAt": "2026-03-11T17:12:53.904Z"
+        },
+        {
+            "name": "GUNDAM HATHAWAY The Sorcery of Nymph Circe",
+            "imageUrl": "https://gundam-official.com/media/hathaway_ii_kv_5318aad7e9/hathaway_ii_kv_5318aad7e9.png",
+            "dateWatched": "2026-03-07",
+            "rating": 4.5,
+            "thoughts": "The 2nd movie of Gundam Hathaway. The story revolves around Hathaway's conflict between idealism and Pragmaticism. The graphic and animation is insanely beautiful. The easter egg during the final fight is also a great payoff for a UC fan. Definitely a must-watch if you are a gundam nerd",
+            "type": "Anime",
+            "id": "1e073885-f894-48ea-886a-7fd9dcc8e61b",
+            "createdAt": "2026-03-07T07:49:17.090Z"
+        },
+        {
+            "name": "A knight of the seven kingdoms",
+            "imageUrl": "https://media.gq.com/photos/69863f3db7871ca31241eaf9/16:9/w_2560%2Cc_limit/dexter-sol-ansell-peter-claffey.jpg",
+            "dateWatched": "2026-03-02",
+            "rating": 5,
+            "thoughts": "A prequel from GOT. GOT/HotD usually tell a story from the Lord or the noble perspective. In contrary, this series is about a commoner Dunk whose master died unexpectedly and in searching of an honor to serve/ to be recognized in 7 Kingdom.",
+            "type": "TV Series",
+            "id": "de186aa8-00fa-43ae-a299-d59a8a391e86",
+            "createdAt": "2026-03-07T07:46:23.842Z"
+        }
+    ]
